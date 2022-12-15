cd7a4dc7c0fae90a0b2b213c51e23e71dc8a63ee
MonikaSharma1233 <118586747+MonikaSharma1233@users.noreply.github.com>
2022-12-15 23:33:33 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ public class Program
         //double lengthofline = CalculateLength.CalculateLengthofLine();
         //Console.WriteLine("The length of the line is :" + lengthofline);
         //UC2
-<<<<<<< HEAD
+
         // CheckEquality_UC2.Compare_Length();
         CompareLines_UC3.CompareLinesLength();
-=======
+
         CheckEquality_UC2.Compare_Length();
->>>>>>> e1d5c3b43f6152af69a81e920e1ee6cb612572bc
+
 
         Console.ReadKey(true);//use for show screen for a some time.
 
