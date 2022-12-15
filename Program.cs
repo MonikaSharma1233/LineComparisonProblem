73f196758f07a232eee05678d741b75d1051e56d
firsttoken <monikasharma12330@gmail.com>
2022-12-15 07:38:43 +0530

    Greating Message

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(String[] strings)
+    {
+        Console.WriteLine("Welcome to Comparison Computation Program");
+    }
+}
