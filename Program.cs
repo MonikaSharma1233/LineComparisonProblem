e8ab5e89b8524dd4b22e8bcd413a2cd7103f38ae
firsttoken <monikasharma12330@gmail.com>
2022-12-15 19:27:36 +0530

    Update_CheckEquality_UC2

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ public class Program
     public static void Main(String[] strings)
     {
         Console.WriteLine("Welcome to Comparison Computation Program");
-        double lengthofline = CalculateLength.CalculateLengthofLine();
-        
-
-        Console.WriteLine("The length of the line is :" + lengthofline);
+        //UC1
+        //double lengthofline = CalculateLength.CalculateLengthofLine();
+        //Console.WriteLine("The length of the line is :" + lengthofline);
+        //UC2
+        CheckEquality_UC2.Compare_Length();
 
         Console.ReadKey(true);//use for show screen for a some time.
 
